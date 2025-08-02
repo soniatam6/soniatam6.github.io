d9b8f53c3ac7ef3a0f1b8b5b61eb97de71f3a35f
Sonia <soniatam6@gmail.com>
2025-08-01 21:29:13 -0400

    fix deploy bug

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/soniatam6.github.io/' : '/',
+  base: '/',
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
   server: {
     host: "::",
     port: 8080,
